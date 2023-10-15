a80a2f489395026fcd6eb5f58c61a43491a3c1be
dev-dong <dho0647@naver.com>
2023-10-15 15:36:37 +0900

    server init

---
+++ server/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+const tweets = [
+    {
+        id: 1,
+        text: '드림코딩에서 강의 들으면 너무 좋으다',
+        createdAt: '2021-05-09T04:20:57.000Z',
+        name: 'Bob',
+        username: 'bob',
+        url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+    },
+];
