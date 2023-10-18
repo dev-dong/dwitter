98b72622e5f05720d1c1588fe61c266aaa54435e
dev-dong <dho0647@naver.com>
2023-10-19 02:03:59 +0900

    dwitter add MySQL

--- server/data/auth.js
+++ server/data/auth.js
@@ -1,39 +1,20 @@
-// 12345: $2b$12$38ilTtnWFlDgyUkeOaveuuXZsthknaH/rNAAzv.d4LQ1GQtgCmmhG
-let users = [
-    {
-        id: '1',
-        username: 'bob',
-        password: '$2b$12$38ilTtnWFlDgyUkeOaveuuXZsthknaH/rNAAzv.d4LQ1GQtgCmmhG',
-        name: 'Bob',
-        email: 'bob@gmail.com',
-        url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png'
-    },
-    {
-        id: '2',
-        username: 'dongho',
-        password: '$2b$12$38ilTtnWFlDgyUkeOaveuuXZsthknaH/rNAAzv.d4LQ1GQtgCmmhG',
-        name: 'Dongho',
-        email: 'dho@gmail.com',
-        url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png'
-    },
-];
+import {db} from "../db/database.js";
 
 export async function findByUsername(username) {
-    console.log("Searching for username:", username);  // 확인용 로그
-    const user = users.find(user => {
-        console.log(user);  // 현재 확인 중인 user 객체를 출력
-        return user.username === username;
-    });
-    console.log("Found user:", user);  // 찾은 user 객체를 출력
-    return user;
+    return db.execute('SELECT * FROM users WHERE username = ?', [username])
+        .then(result => result[0][0]);
 }
 
 export async function findById(id) {
-    return users.find(user => user.id === id);
+    return db.execute('SELECT * FROM users WHERE id = ?', [id])
+        .then(result => result[0][0]);
 }
 
 export async function createUser(user) {
-    const created = {...user, id: Date.now().toString()};
-    users.push(created);
-    return created.id;
+    const {username, password, name, email, url} = user;
+
+    // SQL 쿼리를 작성
+    return db.execute('INSERT INTO users (username, password, name, email, url) VALUES  (?, ?, ?, ?, ?)',
+        [user.username, user.password, user.name, user.email, user.url]
+    ).then(result => result[0].insertId); // result[0]은 쿼리 결과, result[0].insertId는 삽입된 데이터의 id
 }
--- server/data/tweet.js
+++ server/data/tweet.js
@@ -1,60 +1,35 @@
-import * as userRepository from './auth.js';
+import {db} from "../db/database.js";
 
-// userRepository 참고한다.
-let tweets = [
-    {
-        id: 1,
-        text: '드림코더분들 화이팅!',
-        createdAt: new Date().toString(),
-        userId: '1'
-    }
-];
+const SELECT_JOIN = 'SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.username, us.name, us.url FROM tweets as tw JOIN users as us ON tw.userId = us.id';
+const ORDER_DESC = 'ORDER BY tw.createdAt DESC';
 
 // 그냥 return을 해도 async 키워드가 붙으면 promise 형태로 변환된다.
 export async function getAll() {
-    return Promise.all(
-        tweets.map(async tweet => {
-            const {username, name, url} = await userRepository.findById(tweet.userId);
-            console.log({...tweet});
-            return {...tweet, username, name, url};
-        })
-    );
+    return db.execute(`${SELECT_JOIN} ${ORDER_DESC}`)
+        .then(result => result[0]);
 }
 
 export async function getAllByUsername(username) {
-    return getAll().then(tweets => tweets.filter(tweet => tweet.username === username));
+    return db.execute(`${SELECT_JOIN} WHERE username=? ${ORDER_DESC}`, [username])
+        .then(result => result[0]);
 }
 
 export async function getById(id) {
-    const found = tweets.find(tweet => tweet.id === id);
-    if (!found) {
-        console.log(`Tweet id ${id} not found`);
-        return null;
-    }
-    const {username, name, url} = await userRepository.findById(found.userId);
-    return {...found, username, name, url};
+    return db.execute(`${SELECT_JOIN} WHERE tw.id=?`, [id])
+        .then(result => result[0][0]);
 }
 
+// 사용자의 정보를 return 해줘야 한다.
 export async function create(text, userId) {
-    const tweet = {
-        id: Date.now().toString(),
-        text,
-        createdAt: new Date(),
-        userId
-    };
-    tweets = [tweet, ...tweets];
-    return getById(tweet.id);
+    return db.execute('INSERT INTO tweets (text, createdAt, userId) VALUES (?, ?, ?)', [text, new Date(), userId])
+        .then(result => getById(result[0].insertId));
 }
 
 export async function update(id, text) {
-    const tweet = tweets.find(tweet => tweet.id === id);
-    if (tweet) {
-        tweet.text = text;
-    }
-    return getById(tweet.id);
+    return db.execute('UPDATE tweets SET text=? WHERE id=?', [text, id])
+        .then(() => getById(id));
 }
 
 export async function remove(id) {
-    tweets = tweets.filter(tweet => tweet.id != id);
-    return tweets;
+    return db.execute('DELETE FROM tweets WHERE id=?', [id]);
 }
